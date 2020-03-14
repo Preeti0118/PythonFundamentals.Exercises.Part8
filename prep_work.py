7b3b999efa47141e1112435cce62aa989f1e6720
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-14 14:39:17 -0400

    updated

--- prep_work.py
+++ prep_work.py
@@ -4,7 +4,7 @@ import stat
 import subprocess
 
 open('./newfolder/newfile2.txt','w+')
-#os.chmod('newfile.txt', stat.S_IREAD)
-#print(os.access('newfile.txt', os.R_OK))
-#os.chmod('newfile.txt', 000)
-#print(os.access('newfile.txt', os.R_OK))
+os.chmod('newfile.txt', stat.S_IREAD)
+print(os.access('newfile.txt', os.R_OK))
+os.chmod('newfile.txt', 000)
+print(os.access('newfile.txt', os.R_OK))
